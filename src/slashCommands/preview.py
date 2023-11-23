c55a57035f406eadaf6fc5bc5636857d96c89adf
Cuh4 <roozzguy@gmail.com>
2023-11-23 08:29:30 +0000

    reformat

--- src/slashCommands/preview.py
+++ src/slashCommands/preview.py
@@ -1,5 +1,5 @@
 # // ---------------------------------------------------------------------
-# // ------- [Welcome Bot] Preview
+# // ------- [Welcome Bot] Preview Slash Command
 # // ---------------------------------------------------------------------
 
 # // ---- Imports
@@ -11,46 +11,41 @@ from helpers import general as helpers
 from helpers import discord as discordHelpers
 
 # // ---- Main
-# // create command
-def command():
-    # // get vars
-    # discord-related
-    client: discord.Client = helpers.globals.get("client")
-    tree: discord.app_commands.CommandTree = helpers.globals.get("commandTree")
+# // get vars
+# discord-related
+client: discord.Client = helpers.globals.get("client")
+tree: discord.app_commands.CommandTree = helpers.globals.get("commandTree")
+
+# // main command
+# slash command
+@tree.command(
+    name = "preview",
+    description = "Preview the welcome message for your server."
+)
+async def command(interaction: discord.Interaction):
+    # // checks
+    # setup
+    checks = helpers.misc.failChecks()
     
-    # // main command
-    # slash command
-    @tree.command(
-        name = "preview",
-        description = "Preview the welcome message for your server."
-    )
-    async def command(interaction: discord.Interaction):
-        # // checks
-        # setup
-        checks = helpers.misc.failChecks()
+    # permissions check
+    if not discordHelpers.utils.hasPermissions(client, interaction.user, ["manage_guild"]):
+        checks.fail("You need `manage_guild` permissions to run this command.")
         
-        # permissions check
-        if not discordHelpers.utils.hasPermissions(client, interaction.user, ["manage_guild"]):
-            checks.fail("You need `manage_guild` permissions to run this command.")
-            
-        # failure message if failed
-        failed, failureMessage = checks.result()
-        
-        if failed:
-            return await interaction.response.send_message(
-                embed = discordHelpers.embeds.failure(failureMessage),
-                ephemeral = True
-            )
-            
-        # // main
-        await events.getSavedEvent("on_member_join").asyncFire(
-            member = interaction.user
-        )
-        
-        await interaction.response.send_message(
-            embed = discordHelpers.embeds.success("Done! A preview of your server's welcome message has been sent in your server."),
+    # failure message if failed
+    failed, failureMessage = checks.result()
+    
+    if failed:
+        return await interaction.response.send_message(
+            embed = discordHelpers.embeds.failure(failureMessage),
             ephemeral = True
         )
-
-# // start command
-command()
+        
+    # // main
+    await events.getSavedEvent("on_member_join").asyncFire(
+        member = interaction.user
+    )
+    
+    await interaction.response.send_message(
+        embed = discordHelpers.embeds.success("Done! A preview of your server's welcome message has been sent in your server."),
+        ephemeral = True
+    )
--- src/slashCommands/setup.py
+++ src/slashCommands/setup.py
@@ -1,5 +1,5 @@
 # // ---------------------------------------------------------------------
-# // ------- [Welcome Bot] Setup
+# // ------- [Welcome Bot] Setup Slash Command
 # // ---------------------------------------------------------------------
 
 # // ---- Imports
@@ -10,41 +10,36 @@ from helpers import general as helpers
 from helpers import discord as discordHelpers
 
 # // ---- Main
-# // create command
-def command():
-    # // get vars
-    # discord-related
-    client: discord.Client = helpers.globals.get("client")
-    tree: discord.app_commands.CommandTree = helpers.globals.get("commandTree")
-    
-    # // main command
-    # slash command
-    @tree.command(
-        name = "setup",
-        description = "Set up your server for the bot."
-    )
-    @discord.app_commands.describe(channel = "The channel that welcome messages will be sent in.")
-    async def command(interaction: discord.Interaction, channel: discord.TextChannel):
-        # // checks
-        # setup
-        checks = helpers.misc.failChecks()
-        
-        # permissions check
-        if not discordHelpers.utils.hasPermissions(client, interaction.user, ["manage_guild"]):
-            checks.fail("You need `manage_guild` permissions to run this command.")
-            
-        # failure message if failed
-        failed, failureMessage = checks.result()
-        
-        if failed:
-            return await interaction.response.send_message(
-                embed = discordHelpers.embeds.failure(failureMessage),
-                ephemeral = True
-            )
-            
-        # // main
-        # show modal
-        await interaction.response.send_modal(ui.modals.setup(interaction.guild, channel))
+# // get vars
+# discord-related
+client: discord.Client = helpers.globals.get("client")
+tree: discord.app_commands.CommandTree = helpers.globals.get("commandTree")
 
-# // start command
-command()
+# // main command
+# slash command
+@tree.command(
+    name = "setup",
+    description = "Set up your server for the bot."
+)
+@discord.app_commands.describe(channel = "The channel that welcome messages will be sent in.")
+async def command(interaction: discord.Interaction, channel: discord.TextChannel):
+    # // checks
+    # setup
+    checks = helpers.misc.failChecks()
+    
+    # permissions check
+    if not discordHelpers.utils.hasPermissions(client, interaction.user, ["manage_guild"]):
+        checks.fail("You need `manage_guild` permissions to run this command.")
+        
+    # failure message if failed
+    failed, failureMessage = checks.result()
+    
+    if failed:
+        return await interaction.response.send_message(
+            embed = discordHelpers.embeds.failure(failureMessage),
+            ephemeral = True
+        )
+        
+    # // main
+    # show modal
+    await interaction.response.send_modal(ui.modals.setup(interaction.guild, channel))
