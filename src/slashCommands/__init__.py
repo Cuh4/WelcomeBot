cb0524e7b21ddc6939ac00a23cd584d4f7060260
Cuh4 <roozzguy@gmail.com>
2023-11-23 08:29:39 +0000

    new command

--- src/slashCommands/__init__.py
+++ src/slashCommands/__init__.py
@@ -6,4 +6,5 @@ def start():
     # importing the commands will automatically start them
     # is this a good way to do things? probably not
     from .preview import command as _
+    from .restart import command as _
     from .setup import command as _
